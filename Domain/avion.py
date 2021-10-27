c2b90b6d9999573a22ab94c072aefb55dc23b71e
Robert Serban <serbanrobert43@yahoo.com>
2021-10-27 18:57:13 +0300

    .

--- Domain/avion.py
+++ Domain/avion.py
@@ -8,43 +8,29 @@ def creeaza_zbor_avion(id, nume, clasa, pret, checkin):
     :param checkin: verificare checkin - string
     :return: un dictionar ce retine datele despre o rezervare a unui bilet de avion
     """
-    return {
-        "id" : id,
-        "nume" : nume,
-        "clasa" : clasa,
-        "pret" : pret,
-        "checkin" :checkin
-    }
+    return [('id', id), ('nume', nume), ('clasa', clasa), ('pret', pret), ('checkin', checkin)]
 def getId(avion):
     """
     ia id-ul rezervarii biletului de avion
     :param avion: un dictionar de tip avion
     :return: id-ul rezervarii - string
     """
-    return avion["id"]
+    return avion[0][1]
 
 def getNume(avion):
-    return avion["nume"]
+    return avion[1][1]
 
 def getClasa(avion):
-    return avion["clasa"]
+    return avion[2][1]
 
 def getPret(avion):
-    return avion["pret"]
+    return avion[3][1]
 
 def getCheckin(avion):
-    return avion["checkin"]
+    return avion[4][1]
 
 def toString(avion):
     return "id: {}, nume: {}, clasa: {}, pret: {}, checkin:{}".format(
-        getId(avion),
-        getNume(avion),
-        getClasa(avion),
-        getPret(avion),
-        getCheckin(avion)
-    )
-def toString(avion):
-    return "Id: {}, Nume: {}, Clasa: {}, Pret: {}, Checkin: {}".format(
         getId(avion),
         getNume(avion),
         getClasa(avion),
